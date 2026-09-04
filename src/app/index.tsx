959504fb396eb7fd10134ec656c8241e54984202
Gabriel Pereira <gabs.pereira0@icloud.com>
2026-09-04 20:26:39 -0300

    initial commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,76 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+export default function Index() {
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
-
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+    <LinearGradient
+      // Background Linear Gradient
+      colors={['#00457D', '#05051F']}
+      style={styles.container}
+    >
+      <View style={styles.innerContainer}>
+        <Image
+          source={require('../../assets/Logo/Logo.png')}
+          style={styles.imageLogo}
+        />
+        <Image
+          source={require('../../assets/Imagens/ilustra1.png')}
+          style={styles.imageIlustra}
+        />
+        <Text style={styles.text}>Boas-Vindas!</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton}>Entrar </Text>
+          <MaterialIcons name='arrow-forward' size={20} color='#01080E' />
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
+  },
+  innerContainer: {
+    alignItems: 'center',
+    gap: 64,
+    paddingVertical: 79,
+    paddingHorizontal: 32,
+    width: '90%',
+  },
+  imageLogo: {
+    height: 45,
+  },
+  imageIlustra: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'contain',
+  },
+  text: {
+    color: 'white',
+    fontSize: 25,
+  },
+  button: {
+    backgroundColor: '#7693FF',
+    borderRadius: 32,
+    width: '100%',
+    height: 40,
     flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
     alignItems: 'center',
     justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
   },
-  title: {
-    textAlign: 'center',
+  textButton: {
+    color: '#01080E',
+    fontWeight: '600',
+    fontSize: 20,
+    marginRight: 10,
+    gap: 10,
   },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+  icon: {
+    width: 20,
+    height: 20,
   },
 });
