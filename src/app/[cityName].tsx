e3b3017104a6f17a859ea564f2a333c6241fa944
Gabriel Pereira <gabs.pereira0@icloud.com>
2026-09-08 21:25:20 -0300

    adiciona chamada da API e inicia estilização da página de detalhes da cidade

--- src/app/[cityName].tsx
+++ src/app/[cityName].tsx
@@ -1,18 +1,66 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { useLocalSearchParams } from 'expo-router/build/hooks';
-import { View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const CityDetails = () => {
   const { cityName } = useLocalSearchParams();
-  console.log(cityName);
+
+  const [cityDetails, setCityDetails] = useState<any>(null);
+
+  const handleData = async () => {
+    try {
+      const response = await fetch('https://climapp-api.vercel.app/api/');
+      const responseJson = await response.json();
+      const city = responseJson.find(
+        (cityData: { city: string }) => cityData.city === cityName,
+      );
+      setCityDetails(city);
+    } catch (error) {
+      console.error('Error fetching city data:', error);
+    }
+  };
+
+  useEffect(() => {
+    handleData();
+  }, []);
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Testando nova pagina</Text>
-      </View>
-    </SafeAreaView>
+    <LinearGradient
+      // Background Linear Gradient
+      colors={['#00457D', '#05051F']}
+      style={styles.container}
+    >
+      <SafeAreaView>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>{cityDetails?.city}</Text>
+        </View>
+        <View>
+          <Text> teste</Text>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 40,
+  },
+  headerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    fontFamily: 'Montserrat_500Medium',
+    fontWeight: 'bold',
+  },
+});
 
 export default CityDetails;
